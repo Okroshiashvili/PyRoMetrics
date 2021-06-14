e2daa3cff6f7af83b72724b74ec4e1e565345f14
Okroshiashvili <n.okroshiashvili@gmail.com>
2021-06-14 17:18:44 +0400

    Add type annotations and docstrings

--- pyrometrics/metrics.py
+++ pyrometrics/metrics.py
@@ -1,43 +1,107 @@
 import numpy as np
 
-EPSILON = 1e-10
+from pyrometrics.helpers import _error, _percentage_error
 
 
-def _error(actual, predicted):
+def mse(actual: np.ndarray, predicted: np.ndarray):
+    """
+    MSE - Mean Square Error
 
-    return actual - predicted
+    This is an arithmetic average of squared error.
 
+    Args:
+        actual (np.ndarray): array of actual values
+        predicted (np.ndarray): array of predicted values
 
-def _percentage_error(actual, predicted):
-
-    return _error(actual, predicted) / (actual + EPSILON)
-
-
-def mse(actual, predicted):
+    Returns:
+        Mean Square Error
+    """
 
     return np.mean(np.square(_error(actual, predicted)))
 
 
-def rmse(actual, predicted):
+def rmse(actual: np.ndarray, predicted: np.ndarray):
+    """
+    RMSE - Root Mean Square Error
+
+    This is a square root from mean of squared error.
+
+    Args:
+        actual (np.ndarray): array of actual values
+        predicted (np.ndarray): array of predicted values
+
+    Returns:
+        Root Mean Square Error
+    """
 
     return np.sqrt(mse(actual, predicted))
 
 
-def me(actual, predicted):
+def me(actual: np.ndarray, predicted: np.ndarray):
+    """
+    ME - Mean Error
+
+    This is an arithmetic average of error
+
+    Args:
+        actual (np.ndarray): array of actual values
+        predicted (np.ndarray): array of predicted values
+
+    Returns:
+        Mean Error
+    """
 
     return np.mean(_error(actual, predicted))
 
 
-def mae(actual, predicted):
+def mae(actual: np.ndarray, predicted: np.ndarray):
+    """
+    MAE - Mean Absolute Error
+
+    This is an arithmetic average of absolute value of error
+
+    Args:
+        actual (np.ndarray): array of actual values
+        predicted (np.ndarray): array of predicted values
+
+    Returns:
+        Mean Absolute Error
+    """
 
     return np.mean(np.abs(_error(actual, predicted)))
 
 
-def mpe(actual, predicted):
+def mpe(actual: np.ndarray, predicted: np.ndarray):
+    """
+    MPE - Mean Percentage Error
+
+    This is an arithmetic average of percentage error
+
+    Args:
+        actual (np.ndarray): array of actual values
+        predicted (np.ndarray): array of predicted values
+
+    Returns:
+        Mean Percentage Error
+    """
 
     return np.mean(_percentage_error(actual, predicted))
 
 
-def mape(actual, predicted):
+def mape(actual: np.ndarray, predicted: np.ndarray):
+    """
+    MAPE - Mean Absolute Percentage Error
+
+    This is an arithmetic average of absolute value of percentage error
+
+    Note: The result is not multiplied by 100
+
+    Args:
+        actual (np.ndarray): array of actual values
+        predicted (np.ndarray): array of predicted values
+
+    Returns:
+        Mean Absolute Percentage Error
+    """
 
     return np.mean(np.abs(_percentage_error(actual, predicted)))
