9ac5618517412832825989cc3dc83a136056c3ee
Okroshiashvili <n.okroshiashvili@gmail.com>
2021-06-27 14:39:21 +0400

    update conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,11 @@
-import pandas as pd
+import numpy as np
 import pytest
 
 
 @pytest.fixture
 def data():
 
-    data = {
-        "actual": [544, 422, 269, 241, 424, 376, 464, 572, 254, 296],
-        "predicted": [529, 474, 301, 325, 346, 381, 461, 544, 280, 310],
-    }
+    actual = np.array([544, 422, 269, 241, 424, 376, 464, 572, 254, 296])
+    predicted = np.array([529, 474, 301, 325, 346, 381, 461, 544, 280, 310])
 
-    df = pd.DataFrame(data)
-
-    return df
+    return actual, predicted
